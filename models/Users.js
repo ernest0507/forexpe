a670e206a33231e585cf19c6c16fb7499de12dbf
ergoog <ergoog51@gmail.com>
2023-07-12 17:40:50 +0700

    first commit

---
+++ models/Users.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataType) => {
+    const Users = sequelize.define("Users", {
+        username: {
+            type: DataType.STRING, 
+            allowNull: false, 
+        },
+
+        email: {
+            type: DataType.STRING, 
+            allowNull: false, 
+        },
+
+        password: {
+            type: DataType.STRING, 
+            allowNull: false, 
+        },
+
+    })
+    return Users
+}
